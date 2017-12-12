4a6ff44518e638bbbc75a94f3d467a814a00273d
yhe <yhe@sina.com>
2017-12-12 23:17:50 +0800

    Gaussian Elimination

---
+++ Gaussian_Elimination.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby -w
+
+print("Gauss elimination with maximal column pivoting algorithm for linear equations.\n");
+
+# linear equations: AX = B
+# equation count
+N = 3;
+# x count
+M = 3;
+
+mat_A = [[1, 1, 1],
+         [1, -1, 1],
+         [1, -1, 0]];
+mat_B = [1, 1, 0];
+
+row = 0;
+col = 0;
+while(row < N)
+    (N - row).times {|i|
+    }
+end
+
+print mat_A, "\n";
+print mat_B, "\n";
