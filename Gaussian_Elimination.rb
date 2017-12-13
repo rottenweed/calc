b01e94acd39f3ff07322b8067f7e7bc12b894f49
yhe <yhe@sina.com>
2017-12-13 23:05:29 +0800

    modified.

--- Gaussian_Elimination.rb
+++ Gaussian_Elimination.rb
@@ -2,6 +2,8 @@
 
 print("Gauss elimination with maximal column pivoting algorithm for linear equations.\n");
 
+# the lowest limit
+ZERO = 1e-8;
 # linear equations: AX = B
 # equation count
 N = 3;
@@ -13,11 +15,27 @@ mat_A = [[1, 1, 1],
          [1, -1, 0]];
 mat_B = [1, 1, 0];
 
+# [row, col]: the left-upper element of the sub-matrix which needs to be eliminated
 row = 0;
 col = 0;
 while(row < N)
+    max_index = row;
+    max = mat_A[row][col].abs;
     (N - row).times {|i|
+        if(mat_A[i + row][col].abs > max)
+            max = mat_A[i + row][col];
+            max_index = i;
+        end
     }
+    if(max_index != row)
+        ratio = mat_A[max_index][col];
+        M.times {|j|
+            temp = mat_A[row][j];
+            mat_A[row][j] = mat_A[max_index][j];
+            mat_A[max_index][j] = temp;
+        }
+    end
+    row += 1;
 end
 
 print mat_A, "\n";
