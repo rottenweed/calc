a294a19b023279fbcb69580725c3f88deeb2af36
He Yong <yhe@vip.sina.com>
2018-05-14 17:32:26 +0800

    Fix a bug in population. Write csv file as record.

--- y_dna/extinct.rb
+++ y_dna/extinct.rb
@@ -13,7 +13,10 @@ sample = Array.new(N) {|i| i;};
 sum = N;
 sum_last = N;
 count = Array.new(N, 0);
-survive = 0;
+survive = 0;        # survived count of initial person
+ratio_10 = 0.0;     # percent of the top 10
+record_file = File.open("extinct.csv", "w");
+
 # random born between generation
 GEN.times { |gen|
     sum = (sum_last * RATIO).to_i;  # population increase
@@ -28,7 +31,7 @@ GEN.times { |gen|
         pos = (rand() * sum_last).to_i;
         sample_next[i] = sample[pos];
     }
-    N.times {|i|
+    sum.times {|i|
         count[sample_next[i]] += 1;
     }
     N.times {|i|
@@ -36,10 +39,13 @@ GEN.times { |gen|
     }
     sample = sample_next;
     sum_last = sum;
+    # record the data in every generation
+    ratio_10 = (count.sort)[N - MAX_COUNT, MAX_COUNT].sum * 100.0 / sum;
+    record_file.print("#{gen},#{sum},#{survive},#{ratio_10}\n");
 }
 
-count_sort = count.sort;
+record_file.close;
 print("total = #{sum}, survive = #{survive}\n");
-printf("sum of top 10: %.2f\%\n", count_sort[N - MAX_COUNT, MAX_COUNT].sum * 100.0 / sum);
-print(count_sort[N - MAX_COUNT, MAX_COUNT], "\n");
+printf("sum of top 10: %.2f\%\n", ratio_10);
+print((count.sort)[N - MAX_COUNT, MAX_COUNT], "\n");
 
