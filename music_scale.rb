6f6a940b8d4171a519e1f5fe9ed600454e2bd0ba
yhe <yhe@sina.com>
2018-07-23 10:46:36 +0800

    Add music cent

--- music_scale.rb
+++ music_scale.rb
@@ -3,13 +3,14 @@
 print("Calculate musical scale\n");
 
 SCALE_CNT = 12;
+LN2 = Math.log(2);
 average12 = Array.new(12, 1.0);
 step5 = Array.new(12, 1.0);
 pure7 = Array.new(12, 1.0);
 
 # generate 12-scale average
 SCALE_CNT.times {|i|
-    average12[i] = Math.exp(i * Math.log(2) / 12);
+    average12[i] = Math.exp(i * LN2 / 12);
 }
 # generate 5-step scale
 step5[7] = step5[0] * 1.5;
@@ -21,5 +22,19 @@ pure7[4] = 5.0 / 4.0;
 pure7[7] = 6.0 / 4.0;
 
 SCALE_CNT.times {|i|
-    printf("%2d: %1.7f, %1.7f, %1.7f\n", i, average12[i], step5[i], pure7[i]);
+    printf("%2d: %1.7f, ", i, average12[i]);
+    printf("%6.1f, ", 1200.0 * Math.log(average12[i]) / LN2);
+    printf("%1.7f, ", step5[i]);
+    if(step5[i] != 1.0)
+        printf("%6.1f, ", 1200.0 * Math.log(step5[i]) / LN2);
+    else
+        print(" " * 6, ", ");
+    end
+    printf("%1.7f", pure7[i]);
+    if(pure7[i] != 1.0)
+        printf("%6.1f, ", 1200.0 * Math.log(pure7[i]) / LN2);
+    else
+        print(" " * 6, ", ");
+    end
+    print("\n");
 }
