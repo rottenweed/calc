f23ed26600d98b437291e1aecc1ddeae1618d292
yhe <yhe@sina.com>
2018-09-09 16:14:56 +0800

    little modify on CRC-16

--- crc.rb
+++ crc.rb
@@ -1,34 +1,34 @@
 #! /usr/bin/ruby -w
 
-print("Cal CRC function.\n");
-crc_code = 0x8005;
-crc_len = 16;
-crc_bit_cnt = 8;
+print("Cal CRC parallel function.\n");
+CRC_CODE = 0x8005;  //CRC-16: x^16 + x^15 + x^2 + 1
+CRC_LEN = 16;
+CRC_BIT_CNT = 8;
 
-crc_msb = 1 << crc_len;
+crc_msb = 1 << CRC_LEN;
 crc_mask = crc_msb - 1;
-crc_bit_result = Array.new(crc_len, 0);
+crc_bit_result = Array.new(CRC_LEN, 0);
 
-crc_len.times {|i|
+CRC_LEN.times {|i|
     crc_value = 1 << i;
-    crc_bit_cnt.times {|j|
+    CRC_BIT_CNT.times {|j|
         crc_value <<= 1;
         if((crc_value & crc_msb) == crc_msb)
-            crc_value ^= crc_code;
+            crc_value ^= CRC_CODE;
         end
         crc_value &= crc_mask;
     }
     crc_bit_result[i] = crc_value;
 }
-
+=begin
 crc_bit_result.each {|i|
     printf("%04x\n", i);
 }
-
+=end
 print("Corresponding bits as:\n");
-(crc_len - 1).downto(0) {|i|
+(CRC_LEN - 1).downto(0) {|i|
     printf("bit[%d]: ", i);
-    (crc_len - 1).downto(0) {|j|
+    (CRC_LEN - 1).downto(0) {|j|
         if(crc_bit_result[j] & (1 << i) == (1 << i))
             printf("%d, ", j);
         end
