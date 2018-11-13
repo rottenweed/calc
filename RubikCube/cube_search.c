bf3d11511d5f8ec4f776e40f462b7390acd2ac7e
He Yong <yhe@vip.sina.com>
2018-11-13 19:02:30 +0800

    Modify array.

--- RubikCube/cube_search.c
+++ RubikCube/cube_search.c
@@ -1,130 +1,133 @@
 #include <stdio.h>
 
-unsigned char top[] = {'w', 'w', 'w',   //z
+unsigned char cube_start[6][9] = {
+                        {'w', 'w', 'w', //top, z+
                         'w', 'w', 'w',
-                        'w', 'w', 'w'};
-unsigned char hor0[] = {'r', 'r', 'r',  //x
+                        'w', 'w', 'w'},
+                        {'r', 'r', 'r', //hor0, x+
                         'r', 'r', 'r',
-                        'r', 'r', 'r'};
-unsigned char hor1[] = {'b', 'b', 'b',  //y
+                        'r', 'r', 'r'},
+                        {'b', 'b', 'b', //hor1, y+
                         'b', 'b', 'b',
-                        'b', 'b', 'b'};
-unsigned char hor2[] = {'o', 'o', 'o',
+                        'b', 'b', 'b'},
+                        {'o', 'o', 'o', //hor2
                         'o', 'o', 'o',
-                        'o', 'o', 'o'};
-unsigned char hor3[] = {'g', 'g', 'g',
+                        'o', 'o', 'o'},
+                        {'g', 'g', 'g', //hor3
                         'g', 'g', 'g',
-                        'g', 'g', 'g'};
-unsigned char bottom[] = {'y', 'y', 'y',
+                        'g', 'g', 'g'},
+                        {'y', 'y', 'y', //bottom
                         'y', 'y', 'y',
-                        'y', 'y', 'y'};
+                        'y', 'y', 'y'}
+};
 
-void rotate(unsigned char rotate_type)
+void rotate(unsigned char rotate_type, unsigned char *cube)
 {
     unsigned char temp[9];
+/*
     switch(rotate_type) {
         case 0: //x = -1
-            temp[0] = top[0]; temp[1] = top[1]; temp[2] = top[2];
-            top[0] = hor1[2]; top[1] = hor1[5]; top[2] = hor1[8];
-            hor1[2] = bottom[8]; hor1[5] = bottom[7]; hor1[8] = bottom[6];
-            bottom[8] = hor3[6]; bottom[7] = hor3[3]; bottom[6] = hor3[0];
-            hor3[6] = temp[0]; hor3[3] = temp[1]; hor3[0] = temp[2];
+            temp[0] = cube[0][0]; temp[1] = cube[0][1]; temp[2] = cube[0][2];
+            cube[0][0] = cube[2][2]; cube[0][1] = cube[2][5]; cube[0][2] = cube[2][8];
+            cube[2][2] = cube[5][8]; cube[2][5] = cube[5][7]; cube[2][8] = cube[5][6];
+            cube[5][8] = cube[4][6]; cube[5][7] = cube[4][3]; cube[5][6] = cube[4][0];
+            cube[4][6] = temp[0]; cube[4][3] = temp[1]; cube[4][0] = temp[2];
             //rotate a layer
-            temp[0] = hor2[0]; temp[1] = hor2[1]; temp[2] = hor2[2];
-            temp[3] = hor2[3]; temp[4] = hor2[4]; temp[5] = hor2[5];
-            temp[6] = hor2[6]; temp[7] = hor2[7]; temp[8] = hor2[8];
-            hor2[0] = temp[6]; hor2[3] = temp[7];
-            hor2[6] = temp[8]; hor2[7] = temp[5];
-            hor2[8] = temp[2]; hor2[5] = temp[1];
-            hor2[2] = temp[0]; hor2[1] = temp[3];
+            temp[0] = cube[3][0]; temp[1] = cube[3][1]; temp[2] = cube[3][2];
+            temp[3] = cube[3][3]; temp[4] = cube[3][4]; temp[5] = cube[3][5];
+            temp[6] = cube[3][6]; temp[7] = cube[3][7]; temp[8] = cube[3][8];
+            cube[3][0] = temp[6]; cube[3][3] = temp[7];
+            cube[3][6] = temp[8]; cube[3][7] = temp[5];
+            cube[3][8] = temp[2]; cube[3][5] = temp[1];
+            cube[3][2] = temp[0]; cube[3][1] = temp[3];
             break;
         case 1: //x = 1
-            temp[0] = top[6]; temp[1] = top[7]; temp[2] = top[8];
-            top[6] = hor1[0]; top[7] = hor1[3]; top[8] = hor1[6];
-            hor1[0] = bottom[2]; hor1[3] = bottom[1]; hor1[6] = bottom[0];
-            bottom[2] = hor3[8]; bottom[1] = hor3[5]; bottom[0] = hor3[2];
-            hor3[8] = temp[0]; hor3[5] = temp[1]; hor3[2] = temp[2];
+            temp[0] = cube[0][6]; temp[1] = cube[0][7]; temp[2] = cube[0][8];
+            cube[0][6] = cube[2][0]; cube[0][7] = cube[2][3]; cube[0][8] = cube[2][6];
+            cube[2][0] = cube[5][2]; cube[2][3] = cube[5][1]; cube[2][6] = cube[5][0];
+            cube[5][2] = cube[4][8]; cube[5][1] = cube[4][5]; cube[5][0] = cube[4][2];
+            cube[4][8] = temp[0]; cube[4][5] = temp[1]; cube[4][2] = temp[2];
             //rotate a layer
-            temp[0] = hor0[0]; temp[1] = hor0[1]; temp[2] = hor0[2];
-            temp[3] = hor0[3]; temp[4] = hor0[4]; temp[5] = hor0[5];
-            temp[6] = hor0[6]; temp[7] = hor0[7]; temp[8] = hor0[8];
-            hor0[0] = temp[2]; hor0[3] = temp[1];
-            hor0[6] = temp[0]; hor0[7] = temp[3];
-            hor0[8] = temp[6]; hor0[5] = temp[7];
-            hor0[2] = temp[8]; hor0[1] = temp[5];
+            temp[0] = cube[1][0]; temp[1] = cube[1][1]; temp[2] = cube[1][2];
+            temp[3] = cube[1][3]; temp[4] = cube[1][4]; temp[5] = cube[1][5];
+            temp[6] = cube[1][6]; temp[7] = cube[1][7]; temp[8] = cube[1][8];
+            cube[1][0] = temp[2]; cube[1][3] = temp[1];
+            cube[1][6] = temp[0]; cube[1][7] = temp[3];
+            cube[1][8] = temp[6]; cube[1][5] = temp[7];
+            cube[1][2] = temp[8]; cube[1][1] = temp[5];
             break;
         case 2: //y = -1
-            temp[0] = top[6]; temp[1] = top[3]; temp[2] = top[0];
-            top[6] = hor2[2]; top[3] = hor2[5]; top[0] = hor2[8];
-            hor2[2] = bottom[6]; hor2[5] = bottom[3]; hor2[8] = bottom[0];
-            bottom[6] = hor0[6]; bottom[3] = hor0[3]; bottom[0] = hor0[0];
-            hor0[6] = temp[0]; hor0[3] = temp[1]; hor0[0] = temp[2];
+            temp[0] = cube[0][6]; temp[1] = cube[0][3]; temp[2] = cube[0][0];
+            cube[0][6] = cube[3][2]; cube[0][3] = cube[3][5]; cube[0][0] = cube[3][8];
+            cube[3][2] = cube[5][6]; cube[3][5] = cube[5][3]; cube[3][8] = cube[5][0];
+            cube[5][6] = cube[1][6]; cube[5][3] = cube[1][3]; cube[5][0] = cube[1][0];
+            cube[1][6] = temp[0]; cube[1][3] = temp[1]; cube[1][0] = temp[2];
             //rotate a layer
-            temp[0] = hor3[0]; temp[1] = hor3[1]; temp[2] = hor3[2];
-            temp[3] = hor3[3]; temp[4] = hor3[4]; temp[5] = hor3[5];
-            temp[6] = hor3[6]; temp[7] = hor3[7]; temp[8] = hor3[8];
-            hor3[0] = temp[6]; hor3[3] = temp[7];
-            hor3[6] = temp[8]; hor3[7] = temp[5];
-            hor3[8] = temp[2]; hor3[5] = temp[1];
-            hor3[2] = temp[0]; hor3[1] = temp[3];
+            temp[0] = cube[4][0]; temp[1] = cube[4][1]; temp[2] = cube[4][2];
+            temp[3] = cube[4][3]; temp[4] = cube[4][4]; temp[5] = cube[4][5];
+            temp[6] = cube[4][6]; temp[7] = cube[4][7]; temp[8] = cube[4][8];
+            cube[4][0] = temp[6]; cube[4][3] = temp[7];
+            cube[4][6] = temp[8]; cube[4][7] = temp[5];
+            cube[4][8] = temp[2]; cube[4][5] = temp[1];
+            cube[4][2] = temp[0]; cube[4][1] = temp[3];
             break;
         case 3: //y = 1
-            temp[0] = top[8]; temp[1] = top[5]; temp[2] = top[2];
-            top[8] = hor2[0]; top[5] = hor2[3]; top[2] = hor2[6];
-            hor2[0] = bottom[8]; hor2[3] = bottom[5]; hor2[6] = bottom[2];
-            bottom[8] = hor0[8]; bottom[5] = hor0[5]; bottom[2] = hor0[2];
-            hor0[8] = temp[0]; hor0[5] = temp[1]; hor0[2] = temp[2];
+            temp[0] = cube[0][8]; temp[1] = cube[0][5]; temp[2] = cube[0][2];
+            cube[0][8] = cube[3][0]; cube[0][5] = cube[3][3]; cube[0][2] = cube[3][6];
+            cube[3][0] = cube[5][8]; cube[3][3] = cube[5][5]; cube[3][6] = cube[5][2];
+            cube[5][8] = cube[1][8]; cube[5][5] = cube[1][5]; cube[5][2] = cube[1][2];
+            cube[1][8] = temp[0]; cube[1][5] = temp[1]; cube[1][2] = temp[2];
             //rotate a layer
-            temp[0] = hor1[0]; temp[1] = hor1[1]; temp[2] = hor1[2];
-            temp[3] = hor1[3]; temp[4] = hor1[4]; temp[5] = hor1[5];
-            temp[6] = hor1[6]; temp[7] = hor1[7]; temp[8] = hor1[8];
-            hor1[0] = temp[2]; hor1[3] = temp[1];
-            hor1[6] = temp[0]; hor1[7] = temp[3];
-            hor1[8] = temp[6]; hor1[5] = temp[7];
-            hor1[2] = temp[8]; hor1[1] = temp[5];
+            temp[0] = cube[2][0]; temp[1] = cube[2][1]; temp[2] = cube[2][2];
+            temp[3] = cube[2][3]; temp[4] = cube[2][4]; temp[5] = cube[2][5];
+            temp[6] = cube[2][6]; temp[7] = cube[2][7]; temp[8] = cube[2][8];
+            cube[2][0] = temp[2]; cube[2][3] = temp[1];
+            cube[2][6] = temp[0]; cube[2][7] = temp[3];
+            cube[2][8] = temp[6]; cube[2][5] = temp[7];
+            cube[2][2] = temp[8]; cube[2][1] = temp[5];
             break;
         case 4: //z = -1
-            temp[0] = hor0[8]; temp[1] = hor0[7]; temp[2] = hor0[6];
-            hor0[8] = hor3[8]; hor0[7] = hor3[7]; hor0[6] = hor3[6];
-            hor3[8] = hor2[8]; hor3[7] = hor2[7]; hor3[6] = hor2[6];
-            hor2[8] = hor1[8]; hor2[7] = hor1[7]; hor2[6] = hor1[6];
-            hor1[8] = temp[0]; hor1[7] = temp[1]; hor1[6] = temp[2];
+            temp[0] = cube[1][8]; temp[1] = cube[1][7]; temp[2] = cube[1][6];
+            cube[1][8] = cube[4][8]; cube[1][7] = cube[4][7]; cube[1][6] = cube[4][6];
+            cube[4][8] = cube[3][8]; cube[4][7] = cube[3][7]; cube[4][6] = cube[3][6];
+            cube[3][8] = cube[2][8]; cube[3][7] = cube[2][7]; cube[3][6] = cube[2][6];
+            cube[2][8] = temp[0]; cube[2][7] = temp[1]; cube[2][6] = temp[2];
             //rotate a layer
-            temp[0] = bottom[0]; temp[1] = bottom[1]; temp[2] = bottom[2];
-            temp[3] = bottom[3]; temp[4] = bottom[4]; temp[5] = bottom[5];
-            temp[6] = bottom[6]; temp[7] = bottom[7]; temp[8] = bottom[8];
-            bottom[0] = temp[6]; bottom[3] = temp[7];
-            bottom[6] = temp[8]; bottom[7] = temp[5];
-            bottom[8] = temp[2]; bottom[5] = temp[1];
-            bottom[2] = temp[0]; bottom[1] = temp[3];
+            temp[0] = cube[5][0]; temp[1] = cube[5][1]; temp[2] = cube[5][2];
+            temp[3] = cube[5][3]; temp[4] = cube[5][4]; temp[5] = cube[5][5];
+            temp[6] = cube[5][6]; temp[7] = cube[5][7]; temp[8] = cube[5][8];
+            cube[5][0] = temp[6]; cube[5][3] = temp[7];
+            cube[5][6] = temp[8]; cube[5][7] = temp[5];
+            cube[5][8] = temp[2]; cube[5][5] = temp[1];
+            cube[5][2] = temp[0]; cube[5][1] = temp[3];
             break;
         case 5: //z = 1
-            temp[0] = hor0[2]; temp[1] = hor0[1]; temp[2] = hor0[0];
-            hor0[2] = hor3[2]; hor0[1] = hor3[1]; hor0[0] = hor3[0];
-            hor3[2] = hor2[2]; hor3[1] = hor2[1]; hor3[0] = hor2[0];
-            hor2[2] = hor1[2]; hor2[1] = hor1[1]; hor2[0] = hor1[0];
-            hor1[2] = temp[0]; hor1[1] = temp[1]; hor1[0] = temp[2];
+            temp[0] = cube[1][2]; temp[1] = cube[1][1]; temp[2] = cube[1][0];
+            cube[1][2] = cube[4][2]; cube[1][1] = cube[4][1]; cube[1][0] = cube[4][0];
+            cube[4][2] = cube[3][2]; cube[4][1] = cube[3][1]; cube[4][0] = cube[3][0];
+            cube[3][2] = cube[2][2]; cube[3][1] = cube[2][1]; cube[3][0] = cube[2][0];
+            cube[2][2] = temp[0]; cube[2][1] = temp[1]; cube[2][0] = temp[2];
             //rotate a layer
-            temp[0] = top[0]; temp[1] = top[1]; temp[2] = top[2];
-            temp[3] = top[3]; temp[4] = top[4]; temp[5] = top[5];
-            temp[6] = top[6]; temp[7] = top[7]; temp[8] = top[8];
-            top[0] = temp[2]; top[3] = temp[1];
-            top[6] = temp[0]; top[7] = temp[3];
-            top[8] = temp[6]; top[5] = temp[7];
-            top[2] = temp[8]; top[1] = temp[5];
+            temp[0] = cube[0][0]; temp[1] = cube[0][1]; temp[2] = cube[0][2];
+            temp[3] = cube[0][3]; temp[4] = cube[0][4]; temp[5] = cube[0][5];
+            temp[6] = cube[0][6]; temp[7] = cube[0][7]; temp[8] = cube[0][8];
+            cube[0][0] = temp[2]; cube[0][3] = temp[1];
+            cube[0][6] = temp[0]; cube[0][7] = temp[3];
+            cube[0][8] = temp[6]; cube[0][5] = temp[7];
+            cube[0][2] = temp[8]; cube[0][1] = temp[5];
             break;
         default:
             break;
-    }
+    }*/
 }
 
-void print_cube(void)
+void print_cube(unsigned char *cube)
 {
     int i;
 
     printf("Top layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", top[i]);
+        printf("%c", cube[0 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -132,7 +135,7 @@ void print_cube(void)
     }
     printf("Horizon0 layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", hor0[i]);
+        printf("%c", cube[1 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -140,7 +143,7 @@ void print_cube(void)
     }
     printf("Horizon1 layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", hor1[i]);
+        printf("%c", cube[2 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -148,7 +151,7 @@ void print_cube(void)
     }
     printf("Horizon2 layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", hor2[i]);
+        printf("%c", cube[3 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -156,7 +159,7 @@ void print_cube(void)
     }
     printf("Horizon3 layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", hor3[i]);
+        printf("%c", cube[4 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -164,7 +167,7 @@ void print_cube(void)
     }
     printf("Bottom layer:\n");
     for(i = 0; i < 9; i ++) {
-        printf("%c", bottom[i]);
+        printf("%c", cube[5 * 9 + i]);
         if((i % 3) == 2)
             printf("\n");
         else
@@ -172,63 +175,51 @@ void print_cube(void)
     }
 }
 
+int equal_start(unsigned char *cube)
+{
+    int i, j;
+
+    for(i = 0; i < 6; i ++)
+        for(j = 0; j < 9; j ++) {
+            if(cube[i * 9 + j] != cube_start[i][j])
+                return 0;
+        }
+    return 1;
+}
+
 int main(void)
 {
     unsigned char c0, c1, c2;
-    FILE *cube_start;
-    int i;
+    FILE *file_cube_start;
+    int i, j;
+    unsigned char stack_rotate[1000];   //stack to save 6-branch searching tree
+    unsigned char depth, depth_limit;
+    unsigned char record[64];   //as cube data
 
     printf("Search for Rubik's cube.\n");
 
-    cube_start = fopen("start.txt", "r");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        top[i * 3] = c0;
-        top[i * 3 + 1] = c1;
-        top[i * 3 + 2] = c2;
+    //Initiate the start cube from file.
+    file_cube_start = fopen("start.txt", "r");
+    for(i = 0; i < 6; i ++) {
+        for(j = 0; j < 3; j ++) {
+            fscanf(file_cube_start, "%c %c %c\n", &c0, &c1, &c2);
+            cube_start[i][j * 3] = c0;
+            cube_start[i][j * 3 + 1] = c1;
+            cube_start[i][j * 3 + 2] = c2;
+        }
+        if(i != 5)
+            fscanf(file_cube_start, "\n");
     }
-    fscanf(cube_start, "\n");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        hor0[i * 3] = c0;
-        hor0[i * 3 + 1] = c1;
-        hor0[i * 3 + 2] = c2;
-    }
-    fscanf(cube_start, "\n");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        hor1[i * 3] = c0;
-        hor1[i * 3 + 1] = c1;
-        hor1[i * 3 + 2] = c2;
-    }
-    fscanf(cube_start, "\n");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        hor2[i * 3] = c0;
-        hor2[i * 3 + 1] = c1;
-        hor2[i * 3 + 2] = c2;
-    }
-    fscanf(cube_start, "\n");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        hor3[i * 3] = c0;
-        hor3[i * 3 + 1] = c1;
-        hor3[i * 3 + 2] = c2;
-    }
-    fscanf(cube_start, "\n");
-    for(i = 0; i < 3; i ++) {
-        fscanf(cube_start, "%c %c %c\n", &c0, &c1, &c2);
-        bottom[i * 3] = c0;
-        bottom[i * 3 + 1] = c1;
-        bottom[i * 3 + 2] = c2;
-    }
+    //Save the start value.
+    for(i = 0; i < 6; i ++)
+        for(j = 0; j < 9; j ++)
+            record[i * 9 + j] = cube_start[i][j];
+    print_cube(record);
 
-    print_cube();
-    rotate(4);
-    rotate(5);
-    print_cube();
+//    rotate(0, record);
+//    printf("%d\n", equal_start());
 
-    fclose(cube_start);
+    fclose(file_cube_start);
     return 0;
 }
 
